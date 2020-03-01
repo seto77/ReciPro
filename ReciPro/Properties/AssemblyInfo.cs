fb9b22a1bd5958670076a302bb459bfb65f2a5ff
seto <seto77@gmail.com>
2020-03-01 17:23:00 +0900

    20200301_3

--- ReciPro/Properties/AssemblyInfo.cs
+++ ReciPro/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2020.3.1.0801")]
-[assembly: AssemblyFileVersion("2020.3.1.0801")]
+[assembly: AssemblyVersion("2020.3.1.0820")]
+[assembly: AssemblyFileVersion("2020.3.1.0820")]
