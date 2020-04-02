4e55c52d48b83373930a0b59bdd6bdbb3dfb4c22
seto <seto77@gmail.com>
2020-04-02 15:15:54 +0900

    manual

--- ReciPro/Properties/AssemblyInfo.cs
+++ ReciPro/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2020.4.1.1100")]
-[assembly: AssemblyFileVersion("2020.4.1.1100")]
+[assembly: AssemblyVersion("2020.4.2.0611")]
+[assembly: AssemblyFileVersion("2020.4.2.0611")]
