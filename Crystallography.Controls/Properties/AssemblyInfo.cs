6fcfcf6842f5f98a1be36ee637aca218bd447218
seto <seto77@gmail.com>
2020-08-19 22:07:43 +0900

    update

--- Crystallography.Controls/Properties/AssemblyInfo.cs
+++ Crystallography.Controls/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.8.19.1223")]
-[assembly: AssemblyFileVersion("2020.8.19.1223")]
+[assembly: AssemblyVersion("2020.8.19.1242")]
+[assembly: AssemblyFileVersion("2020.8.19.1242")]
