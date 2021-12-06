261b9fe58b7d0818da8353f3fbcf6f884f49f84d
seto <seto77@gmail.com>
2021-12-06 09:21:04 +0900

    update

--- Crystallography/Crystal/CrystalGeometry.cs
+++ Crystallography/Crystal/CrystalGeometry.cs
@@ -1,89 +1,88 @@
 ﻿using MathNet.Numerics.LinearAlgebra.Double;
 using System;
 
-namespace Crystallography
+namespace Crystallography;
+
+public static class CrystalGeometry
 {
-    public static class CrystalGeometry
+    public static double[] GetErrorTriclinic(Plane[] plane)
     {
-        public static double[] GetErrorTriclinic(Plane[] plane)
+        double a, b, c, alpha, beta, gamma, v;
+        double a_err, b_err, c_err, alpha_err, beta_err, gamma_err, v_err;
+        a = b = c = alpha = beta = gamma = v = 0;
+        a_err = b_err = c_err = alpha_err = beta_err = gamma_err = v_err = -1;
+
+        var mQ = new DenseMatrix(plane.Length, 6);
+        var mA = new DenseMatrix(plane.Length, 1);
+        var mW = new DenseMatrix(plane.Length, plane.Length);
+        for (int i = 0; i < plane.Length; i++)
         {
-            double a, b, c, alpha, beta, gamma, v;
-            double a_err, b_err, c_err, alpha_err, beta_err, gamma_err, v_err;
-            a = b = c = alpha = beta = gamma = v = 0;
-            a_err = b_err = c_err = alpha_err = beta_err = gamma_err = v_err = -1;
-
-            var mQ = new DenseMatrix(plane.Length, 6);
-            var mA = new DenseMatrix(plane.Length, 1);
-            var mW = new DenseMatrix(plane.Length, plane.Length);
-            for (int i = 0; i < plane.Length; i++)
-            {
-                mQ[i, 0] = plane[i].h * plane[i].h;
-                mQ[i, 1] = plane[i].k * plane[i].k;
-                mQ[i, 2] = plane[i].l * plane[i].l;
-                mQ[i, 3] = plane[i].k * plane[i].l;
-                mQ[i, 4] = plane[i].l * plane[i].h;
-                mQ[i, 5] = plane[i].h * plane[i].k;
-                mA[i, 0] = 1 / plane[i].dObs / plane[i].dObs;
-                mW[i, i] = plane[i].Weight;
-            }
-            var mC = (mQ.Transpose() * mW * mQ).Inverse() * mQ.Transpose() * mW * mA;
-            //if (mC.E.GetLength(0) == 0 || double.IsNaN(mC.E[0, 0]))
-            //    return new double[0];
-
-            double A = mC[0, 0], B = mC[1, 0], C = mC[2, 0], U = mC[3, 0], V = mC[4, 0], W = mC[5, 0];
-
-            double vStar = Math.Sqrt(4 * A * B * C - A * U * U - B * V * V - C * W * W + U * V * W) / 2;
-            v = 1 / vStar;
-
-            double h = 4 * B * C - U * U, k = 4 * C * A - V * V, l = 4 * A * B - W * W;
-            double p = V * W - 2 * A * U, q = W * U - 2 * B * V, r = U * V - 2 * C * W;
-
-            double h2 = h * h, h4 = h2 * h2, k2 = k * k, k4 = k2 * k2, l2 = l * l, l4 = l2 * l2;
-            double p2 = p * p, p4 = p2 * p2, q2 = q * q, q4 = q2 * q2, r2 = r * r, r4 = r2 * r2;
-
-            a = Math.Sqrt(h) / vStar / 2;
-            b = Math.Sqrt(k) / vStar / 2;
-            c = Math.Sqrt(l) / vStar / 2;
-            double cosAlpha = p / Math.Sqrt(k * l);
-            double cosBeta = q / Math.Sqrt(l * h);
-            double cosGamma = r / Math.Sqrt(h * k);
-            alpha = Math.Acos(cosAlpha);
-            beta = Math.Acos(cosBeta);
-            gamma = Math.Acos(cosGamma);
-
-            if (plane.Length - 6 > 0)//以下は誤差を計算する部分
-            {
-                var Dev = mA - mQ * mC;//偏差を縦成分にとったベクトル Devの成分の２乗和が ピーク数 - パラメータ数になるはず
-                double sum = 0;
-                for (int i = 0; i < plane.Length; i++)
-                    sum += Dev[i, 0] * Dev[i, 0] * plane[i].Weight;
-                //本当の重み行列は
-                for (int i = 0; i < plane.Length; i++)
-                    mW[i, i] = plane[i].Weight * (plane.Length - 6) / sum;
-                var AlphaInverse = (mQ.Transpose() * mW * mQ).Inverse();//この行列の対角成分がパラメータCの誤差の２乗になる
-
-                double Av = AlphaInverse[0, 0], Bv = AlphaInverse[1, 1], Cv = AlphaInverse[2, 2], Uv = AlphaInverse[3, 3], Vv = AlphaInverse[4, 4], Wv = AlphaInverse[5, 5];
-
-                double ValAinv2 = (Av * h4 + Cv * q4 + Bv * r4 + q2 * r2 * Uv + h2 * q2 * Vv + h2 * r2 * Wv) / h4;
-                double ValBinv2 = (Bv * k4 + Av * r4 + Cv * p4 + r2 * p2 * Vv + k2 * r2 * Wv + k2 * p2 * Uv) / k4;
-                double ValCinv2 = (Cv * l4 + Bv * p4 + Av * q4 + p2 * q2 * Wv + l2 * p2 * Uv + l2 * q2 * Vv) / l4;
-                a_err = Math.Sqrt(ValAinv2) * a * a * a / 2;
-                b_err = Math.Sqrt(ValBinv2) * b * b * b / 2;
-                c_err = Math.Sqrt(ValCinv2) * c * c * c / 2;
-
-                double ValCosAlpha = (Av * (l * r * V + k * q * W) * (l * r * V + k * q * W) + 4 * A * A * ((p2 * Bv + q2 * Wv) / l2 + (r2 * Vv + p2 * Cv) / k2 + Uv) * l2 * k2) / (k * k * k * l * l * l);
-                double ValCosBeta = (Bv * (h * p * W + l * r * U) * (h * p * W + l * r * U) + 4 * B * B * ((q2 * Cv + r2 * Uv) / h2 + (p2 * Wv + q2 * Av) / l2 + Vv) * h2 * l2) / (l * l * l * h * h * h);
-                double ValCosGamma = (Cv * (k * q * U + h * p * V) * (k * q * U + h * p * V) + 4 * C * C * ((r2 * Av + p2 * Vv) / k2 + (q2 * Uv + r2 * Bv) / h2 + Wv) * k2 * h2) / (h * h * h * k * k * k);
-                //y = acos(x) => y'=1/(1-x^2)
-                alpha_err = Math.Sqrt(ValCosAlpha) / (1 - cosAlpha * cosAlpha);
-                beta_err = Math.Sqrt(ValCosBeta) / (1 - cosBeta * cosBeta);
-                gamma_err = Math.Sqrt(ValCosGamma) / (1 - cosGamma * cosGamma);
-
-                double ValVstar2 = (Av * h2 + Bv * k2 + Cv * l2 + p2 * Uv + q2 * Vv + r2 * Wv) / 16;
-                v_err = Math.Sqrt(ValVstar2) * v * v * v / 2;
-            }
-
-            return new double[] { a, b, c, alpha, beta, gamma, v, a_err, b_err, c_err, alpha_err, beta_err, gamma_err, v_err };
+            mQ[i, 0] = plane[i].h * plane[i].h;
+            mQ[i, 1] = plane[i].k * plane[i].k;
+            mQ[i, 2] = plane[i].l * plane[i].l;
+            mQ[i, 3] = plane[i].k * plane[i].l;
+            mQ[i, 4] = plane[i].l * plane[i].h;
+            mQ[i, 5] = plane[i].h * plane[i].k;
+            mA[i, 0] = 1 / plane[i].dObs / plane[i].dObs;
+            mW[i, i] = plane[i].Weight;
         }
+        var mC = (mQ.Transpose() * mW * mQ).Inverse() * mQ.Transpose() * mW * mA;
+        //if (mC.E.GetLength(0) == 0 || double.IsNaN(mC.E[0, 0]))
+        //    return new double[0];
+
+        double A = mC[0, 0], B = mC[1, 0], C = mC[2, 0], U = mC[3, 0], V = mC[4, 0], W = mC[5, 0];
+
+        double vStar = Math.Sqrt(4 * A * B * C - A * U * U - B * V * V - C * W * W + U * V * W) / 2;
+        v = 1 / vStar;
+
+        double h = 4 * B * C - U * U, k = 4 * C * A - V * V, l = 4 * A * B - W * W;
+        double p = V * W - 2 * A * U, q = W * U - 2 * B * V, r = U * V - 2 * C * W;
+
+        double h2 = h * h, h4 = h2 * h2, k2 = k * k, k4 = k2 * k2, l2 = l * l, l4 = l2 * l2;
+        double p2 = p * p, p4 = p2 * p2, q2 = q * q, q4 = q2 * q2, r2 = r * r, r4 = r2 * r2;
+
+        a = Math.Sqrt(h) / vStar / 2;
+        b = Math.Sqrt(k) / vStar / 2;
+        c = Math.Sqrt(l) / vStar / 2;
+        double cosAlpha = p / Math.Sqrt(k * l);
+        double cosBeta = q / Math.Sqrt(l * h);
+        double cosGamma = r / Math.Sqrt(h * k);
+        alpha = Math.Acos(cosAlpha);
+        beta = Math.Acos(cosBeta);
+        gamma = Math.Acos(cosGamma);
+
+        if (plane.Length - 6 > 0)//以下は誤差を計算する部分
+        {
+            var Dev = mA - mQ * mC;//偏差を縦成分にとったベクトル Devの成分の２乗和が ピーク数 - パラメータ数になるはず
+            double sum = 0;
+            for (int i = 0; i < plane.Length; i++)
+                sum += Dev[i, 0] * Dev[i, 0] * plane[i].Weight;
+            //本当の重み行列は
+            for (int i = 0; i < plane.Length; i++)
+                mW[i, i] = plane[i].Weight * (plane.Length - 6) / sum;
+            var AlphaInverse = (mQ.Transpose() * mW * mQ).Inverse();//この行列の対角成分がパラメータCの誤差の２乗になる
+
+            double Av = AlphaInverse[0, 0], Bv = AlphaInverse[1, 1], Cv = AlphaInverse[2, 2], Uv = AlphaInverse[3, 3], Vv = AlphaInverse[4, 4], Wv = AlphaInverse[5, 5];
+
+            double ValAinv2 = (Av * h4 + Cv * q4 + Bv * r4 + q2 * r2 * Uv + h2 * q2 * Vv + h2 * r2 * Wv) / h4;
+            double ValBinv2 = (Bv * k4 + Av * r4 + Cv * p4 + r2 * p2 * Vv + k2 * r2 * Wv + k2 * p2 * Uv) / k4;
+            double ValCinv2 = (Cv * l4 + Bv * p4 + Av * q4 + p2 * q2 * Wv + l2 * p2 * Uv + l2 * q2 * Vv) / l4;
+            a_err = Math.Sqrt(ValAinv2) * a * a * a / 2;
+            b_err = Math.Sqrt(ValBinv2) * b * b * b / 2;
+            c_err = Math.Sqrt(ValCinv2) * c * c * c / 2;
+
+            double ValCosAlpha = (Av * (l * r * V + k * q * W) * (l * r * V + k * q * W) + 4 * A * A * ((p2 * Bv + q2 * Wv) / l2 + (r2 * Vv + p2 * Cv) / k2 + Uv) * l2 * k2) / (k * k * k * l * l * l);
+            double ValCosBeta = (Bv * (h * p * W + l * r * U) * (h * p * W + l * r * U) + 4 * B * B * ((q2 * Cv + r2 * Uv) / h2 + (p2 * Wv + q2 * Av) / l2 + Vv) * h2 * l2) / (l * l * l * h * h * h);
+            double ValCosGamma = (Cv * (k * q * U + h * p * V) * (k * q * U + h * p * V) + 4 * C * C * ((r2 * Av + p2 * Vv) / k2 + (q2 * Uv + r2 * Bv) / h2 + Wv) * k2 * h2) / (h * h * h * k * k * k);
+            //y = acos(x) => y'=1/(1-x^2)
+            alpha_err = Math.Sqrt(ValCosAlpha) / (1 - cosAlpha * cosAlpha);
+            beta_err = Math.Sqrt(ValCosBeta) / (1 - cosBeta * cosBeta);
+            gamma_err = Math.Sqrt(ValCosGamma) / (1 - cosGamma * cosGamma);
+
+            double ValVstar2 = (Av * h2 + Bv * k2 + Cv * l2 + p2 * Uv + q2 * Vv + r2 * Wv) / 16;
+            v_err = Math.Sqrt(ValVstar2) * v * v * v / 2;
+        }
+
+        return new double[] { a, b, c, alpha, beta, gamma, v, a_err, b_err, c_err, alpha_err, beta_err, gamma_err, v_err };
     }
 }
--- Crystallography/Miscellaneous.cs
+++ Crystallography/Miscellaneous.cs
@@ -1,133 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace Crystallography
+namespace Crystallography;
+
+public struct FastSpinLock
 {
-    public struct FastSpinLock
+    private const int SYNC_ENTER = 1;
+    private const int SYNC_EXIT = 0;
+    private int _syncFlag;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Enter()
     {
-        private const int SYNC_ENTER = 1;
-        private const int SYNC_EXIT = 0;
-        private int _syncFlag;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Enter()
+        if (Interlocked.CompareExchange(ref _syncFlag, SYNC_ENTER, SYNC_EXIT) == SYNC_ENTER)
         {
-            if (Interlocked.CompareExchange(ref _syncFlag, SYNC_ENTER, SYNC_EXIT) == SYNC_ENTER)
-            {
-                Spin();
-            }
-            return;
+            Spin();
         }
+        return;
+    }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Exit() => Volatile.Write(ref _syncFlag, SYNC_EXIT);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Exit() => Volatile.Write(ref _syncFlag, SYNC_EXIT);
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private void Spin()
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void Spin()
+    {
+        var spinner = new SpinWait();
+        spinner.SpinOnce();
+        while (Interlocked.CompareExchange(ref _syncFlag, SYNC_ENTER, SYNC_EXIT) == SYNC_ENTER)
         {
-            var spinner = new SpinWait();
             spinner.SpinOnce();
-            while (Interlocked.CompareExchange(ref _syncFlag, SYNC_ENTER, SYNC_EXIT) == SYNC_ENTER)
-            {
-                spinner.SpinOnce();
-            }
         }
     }
+}
 
-    public static class Miscellaneous
-    {
-
-        /// <summary>
-        /// 数字に stとかthみたいな文字を追加した文字列で返す
-        /// </summary>
-        /// <param name="number"></param>
-        /// <returns></returns>
-        public static string Ordinal(this int number)
-        {
-            var ones = number % 10;
-            var tens = Math.Floor(number / 10f) % 10;
-            if (tens == 1)
-                return number + "th";
-            return ones switch
-            {
-                1 => number + "st",
-                2 => number + "nd",
-                3 => number + "rd",
-                _ => number + "th",
-            };
-        }
-
-        /// <summary>
-        /// 有限の数字かどうかを判定する
-        /// </summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
-        public static bool IsFiniteNumber(params double[] d)
-        {
-            foreach (var value in d)
-            {
-                if (double.IsNaN(value)|| double.IsInfinity(value))
-                    return false;
-            }
-            return true;
-        }
-
-        private static bool isDecimalPointCommaFlag = true;
-        private static bool isDecimalPointComma = false;
-
-        public static bool IsDecimalPointComma
-        {
-            get
-            {
-                if (isDecimalPointCommaFlag)
-                {
-                    isDecimalPointComma = double.TryParse("1.000,01", out var temp);
-                    isDecimalPointCommaFlag = false;
-                }
-                return isDecimalPointComma;
-            }
-        }
-
-        public static (int Division, int Modulus) DivMod(int n, int m) => (n / m, n % m);
-
-
-        public static ParallelQuery<int> Sequence(int n) => Enumerable.Range(0, n).ToList().AsParallel();
-
-
-
-
-    }
-
-
+public static class Miscellaneous
+{
 
     /// <summary>
-    /// プロパティグリッドのプロパティの並び順をソート
+    /// 数字に stとかthみたいな文字を追加した文字列で返す
     /// </summary>
-    public class DefinitionOrderTypeConverter : TypeConverter
+    /// <param name="number"></param>
+    /// <returns></returns>
+    public static string Ordinal(this int number)
     {
-        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+        var ones = number % 10;
+        var tens = Math.Floor(number / 10f) % 10;
+        if (tens == 1)
+            return number + "th";
+        return ones switch
         {
-            // TypeDescriptorを使用してプロパティ一覧を取得する
-            var pdc = TypeDescriptor.GetProperties(value, attributes);
-
-            // プロパティ一覧をリフレクションから取得
-            var type = value.GetType();
-            var list = new List<string>();
-            foreach (PropertyInfo propertyInfo in type.GetProperties())
-                list.Add(propertyInfo.Name);
-            // リフレクションから取得した順でソート
-            return pdc.Sort(list.ToArray());
-        }
-
-        /// <summary>
-        /// GetPropertiesをサポートしていることを表明する。
-        /// </summary>
-        public override bool GetPropertiesSupported(ITypeDescriptorContext context) => true;
+            1 => number + "st",
+            2 => number + "nd",
+            3 => number + "rd",
+            _ => number + "th",
+        };
     }
+
+    /// <summary>
+    /// 有限の数字かどうかを判定する
+    /// </summary>
+    /// <param name="d"></param>
+    /// <returns></returns>
+    public static bool IsFiniteNumber(params double[] d)
+    {
+        foreach (var value in d)
+        {
+            if (double.IsNaN(value) || double.IsInfinity(value))
+                return false;
+        }
+        return true;
+    }
+
+    private static bool isDecimalPointCommaFlag = true;
+    private static bool isDecimalPointComma = false;
+
+    public static bool IsDecimalPointComma
+    {
+        get
+        {
+            if (isDecimalPointCommaFlag)
+            {
+                isDecimalPointComma = double.TryParse("1.000,01", out _);
+                isDecimalPointCommaFlag = false;
+            }
+            return isDecimalPointComma;
+        }
+    }
+
+    public static (int Division, int Modulus) DivMod(int n, int m) => (n / m, n % m);
+
+}
+
+
+
+/// <summary>
+/// プロパティグリッドのプロパティの並び順をソート
+/// </summary>
+public class DefinitionOrderTypeConverter : TypeConverter
+{
+    public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+    {
+        // TypeDescriptorを使用してプロパティ一覧を取得する
+        var pdc = TypeDescriptor.GetProperties(value, attributes);
+
+        // プロパティ一覧をリフレクションから取得
+        var type = value.GetType();
+        var list = new List<string>();
+        foreach (PropertyInfo propertyInfo in type.GetProperties())
+            list.Add(propertyInfo.Name);
+        // リフレクションから取得した順でソート
+        return pdc.Sort(list.ToArray());
+    }
+
+    /// <summary>
+    /// GetPropertiesをサポートしていることを表明する。
+    /// </summary>
+    public override bool GetPropertiesSupported(ITypeDescriptorContext context) => true;
 }
