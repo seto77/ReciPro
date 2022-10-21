a81a033144c345c19abc0fb50769c76335bbb67e
seto <seto77@gmail.com>
2022-10-21 11:21:55 +0900

    update

--- ReciPro/Version.cs
+++ ReciPro/Version.cs
@@ -10,7 +10,7 @@ internal static class Version
 
     static public string History =
         "History" +
-        "\r\n ver4.838(2022/10/21)  Added a function to output superstructure." +
+        "\r\n ver4.838(2022/10/21)  Improved compatibility of importing CIF files." +
         "\r\n ver4.837(2022/10/20)  Added a function to output superstructure." +
         "\r\n ver4.836(2022/08/30)  The compiler for C++ code was changed to Clang." +
         "\r\n ver4.835(2022/08/09)  Improved compatibility for reading DM3 format files. " +
