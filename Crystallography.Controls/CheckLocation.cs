1b9830dde13c868b4f5835977b7d4a19abc4fbfe
seto <seto77@gmail.com>
2023-03-22 10:44:02 +0900

    update

--- Crystallography.Controls/CheckLocation.cs
+++ Crystallography.Controls/CheckLocation.cs
@@ -9,26 +9,9 @@ namespace Crystallography.Controls
     {
         public static void Adjust(Form form)
         {
-            double fX = form.Location.X, fY = form.Location.Y, fW = form.Size.Width, fH = form.Size.Height;
+            double fL = form.Bounds.Left, fR = form.Bounds.Right, fT = form.Bounds.Top, fB = form.Bounds.Bottom, fW = form.Size.Width, fH = form.Size.Height;
 
-            var needsAdjust = true;
-            foreach (var scr in Screen.AllScreens)
-            {
-                double bX = scr.Bounds.X, bY = scr.Bounds.Y, bW = scr.Bounds.Width, bH = scr.Bounds.Height;
-                //どれかのウィンドウの中に20%以上含まれていたら、needsAdjustをfalse (=更新の必要なし) にする。
-                if ((fX - bX) / fW > -0.8 && (bX + bW - fX - fW) / fW > -0.8 && (fY - bY) / fH > -0.8 && (bY + bH - fY - fH) / fH > -0.8)
-                    needsAdjust = false;
-            }
-            
-            
-            needsAdjust = !Screen.AllScreens.Any(scr =>
-            {
-                double bX = scr.Bounds.X, bY = scr.Bounds.Y, bW = scr.Bounds.Width, bH = scr.Bounds.Height;
-                //どれかのウィンドウの中に20%以上含まれていたら、needsAdjustをfalse (=更新の必要なし) にする。
-                return ((fX - bX) / fW > -0.8 && (bX + bW - fX - fW) / fW > -0.8 && (fY - bY) / fH > -0.8 && (bY + bH - fY - fH) / fH > -0.8);
-            });
-
-            if (needsAdjust)
+            if (Screen.AllScreens.Any(s => (fL - s.Bounds.Left) / fW < -0.8 || (fR - s.Bounds.Right) / fW < -0.8 || (fT - s.Bounds.Top) / fH < -0.8 || (fB - s.Bounds.Bottom) / fH < -0.8))
             {
                 var scr = Screen.AllScreens.First(e => e.DeviceName == Screen.FromControl(form).DeviceName);
                 form.Location = new Point(scr.Bounds.X + 100, scr.Bounds.Y + 100);
