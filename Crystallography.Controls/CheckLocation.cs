38be774841a2b40fac58bf579c437d6486420c31
seto <seto77@gmail.com>
2023-03-22 10:44:14 +0900

    update

--- Crystallography.Controls/CheckLocation.cs
+++ Crystallography.Controls/CheckLocation.cs
@@ -1,7 +1,6 @@
 ﻿using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using static IronPython.Modules._ast;
 
 namespace Crystallography.Controls
 {
@@ -11,7 +10,7 @@ namespace Crystallography.Controls
         {
             double fL = form.Bounds.Left, fR = form.Bounds.Right, fT = form.Bounds.Top, fB = form.Bounds.Bottom, fW = form.Size.Width, fH = form.Size.Height;
 
-            if (Screen.AllScreens.Any(s => (fL - s.Bounds.Left) / fW < -0.8 || (fR - s.Bounds.Right) / fW < -0.8 || (fT - s.Bounds.Top) / fH < -0.8 || (fB - s.Bounds.Bottom) / fH < -0.8))
+            if (Screen.AllScreens.Any(s => (fL - s.Bounds.Left) / fW < -0.8 || (s.Bounds.Right- fR) / fW < -0.8 || (s.Bounds.Top- fT) / fH < -0.8 || (s.Bounds.Bottom - fB) / fH < -0.8))
             {
                 var scr = Screen.AllScreens.First(e => e.DeviceName == Screen.FromControl(form).DeviceName);
                 form.Location = new Point(scr.Bounds.X + 100, scr.Bounds.Y + 100);
