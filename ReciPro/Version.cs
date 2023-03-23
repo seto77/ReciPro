97f756f5df378e900baeee85dc6c9f4b5dbebbc0
seto <seto77@gmail.com>
2023-03-23 21:23:54 +0900

    Update

--- ReciPro/Version.cs
+++ ReciPro/Version.cs
@@ -10,7 +10,7 @@ internal static class Version
 
     public const string History =
         "History" +
-        "\r\n ver4.855(2023/03/23)  Added a feature to save simulation conditions in HRTEM/STEM simulation." +
+        "\r\n ver4.856(2023/03/23)   Fixed minor GUI bugs on HRTEM/STEM simulation." +
         "\r\n ver4.855(2023/03/23)  Added a feature to save simulation conditions in HRTEM/STEM simulation." +
         "\r\n ver4.854(2023/03/11)  Fixed minor GUI bugs on HRTEM/STEM simulation." +
         "\r\n ver4.853(2023/03/09)  Corrected errors in formulas in STEM simulations. Added LA-CBED caluculation mode." +
