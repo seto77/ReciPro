fe2150a2fe10ad69addedcf4dfcd7d8cce9cc357
seto <seto77@gmail.com>
2024-08-30 21:50:47 +0900

    update

--- ReciPro/FormEBSD.Designer.cs
+++ ReciPro/FormEBSD.Designer.cs
@@ -36,6 +36,17 @@
             buttonViewIsometric = new System.Windows.Forms.Button();
             buttonViewAlongBeam = new System.Windows.Forms.Button();
             paneltTrajectory = new System.Windows.Forms.Panel();
+            graphControl1 = new GraphControl();
+            numericBoxCalcNum = new NumericBox();
+            numericBoxDrawNum = new NumericBox();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            graphControl2 = new GraphControl();
+            statusStrip1 = new System.Windows.Forms.StatusStrip();
+            toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // waveLengthControl1
@@ -95,10 +106,168 @@
             resources.ApplyResources(paneltTrajectory, "paneltTrajectory");
             paneltTrajectory.Name = "paneltTrajectory";
             // 
+            // graphControl1
+            // 
+            graphControl1.AllowMouseOperation = true;
+            graphControl1.AxisLineColor = System.Drawing.Color.Gray;
+            graphControl1.AxisTextColor = System.Drawing.Color.Black;
+            graphControl1.AxisTextFont = new System.Drawing.Font("Segoe UI", 9F);
+            graphControl1.AxisXTextVisible = true;
+            graphControl1.AxisYTextVisible = true;
+            graphControl1.BackgroundColor = System.Drawing.Color.White;
+            graphControl1.BottomMargin = 0D;
+            graphControl1.DivisionLineColor = System.Drawing.Color.LightGray;
+            graphControl1.DivisionLineXVisible = true;
+            graphControl1.DivisionLineYVisible = true;
+            resources.ApplyResources(graphControl1, "graphControl1");
+            graphControl1.DrawingRange = (RectangleD)resources.GetObject("graphControl1.DrawingRange");
+            graphControl1.FixRangeHorizontal = false;
+            graphControl1.FixRangeVertical = false;
+            graphControl1.GraphTitle = "";
+            graphControl1.Interpolation = false;
+            graphControl1.IsIntegerX = false;
+            graphControl1.IsIntegerY = false;
+            graphControl1.LabelX = "X:";
+            graphControl1.LabelY = "Y:";
+            graphControl1.LeftMargin = 0F;
+            graphControl1.LineWidth = 1F;
+            graphControl1.LowerX = 0D;
+            graphControl1.LowerY = 0D;
+            graphControl1.MaximalX = 1D;
+            graphControl1.MaximalY = 1D;
+            graphControl1.MinimalX = 0D;
+            graphControl1.MinimalY = 0D;
+            graphControl1.Mode = GraphControl.DrawingMode.Histogram;
+            graphControl1.MousePositionVisible = true;
+            graphControl1.MousePositionXDigit = -1;
+            graphControl1.MousePositionYDigit = -1;
+            graphControl1.Name = "graphControl1";
+            graphControl1.OriginPosition = new System.Drawing.Point(40, 20);
+            graphControl1.Profile = null;
+            graphControl1.Smoothing = false;
+            graphControl1.UnitX = "";
+            graphControl1.UnitY = "";
+            graphControl1.UpperPanelFont = new System.Drawing.Font("Segoe UI Symbol", 9F);
+            graphControl1.UpperPanelVisible = true;
+            graphControl1.UpperX = 1D;
+            graphControl1.UpperY = 1D;
+            graphControl1.UseLineWidth = true;
+            graphControl1.VerticalLineColor = System.Drawing.Color.Red;
+            graphControl1.XLog = false;
+            graphControl1.YLog = false;
+            // 
+            // numericBoxCalcNum
+            // 
+            numericBoxCalcNum.BackColor = System.Drawing.Color.Transparent;
+            numericBoxCalcNum.DecimalPlaces = 0;
+            resources.ApplyResources(numericBoxCalcNum, "numericBoxCalcNum");
+            numericBoxCalcNum.Maximum = 100000D;
+            numericBoxCalcNum.Minimum = 100D;
+            numericBoxCalcNum.Name = "numericBoxCalcNum";
+            numericBoxCalcNum.RadianValue = 174.53292519943295D;
+            numericBoxCalcNum.RoundErrorAccuracy = -1;
+            numericBoxCalcNum.ShowUpDown = true;
+            numericBoxCalcNum.SmartIncrement = true;
+            numericBoxCalcNum.Value = 10000D;
+            // 
+            // numericBoxDrawNum
+            // 
+            numericBoxDrawNum.BackColor = System.Drawing.Color.Transparent;
+            numericBoxDrawNum.DecimalPlaces = 0;
+            resources.ApplyResources(numericBoxDrawNum, "numericBoxDrawNum");
+            numericBoxDrawNum.Maximum = 100000D;
+            numericBoxDrawNum.Minimum = 100D;
+            numericBoxDrawNum.Name = "numericBoxDrawNum";
+            numericBoxDrawNum.RadianValue = 8.7266462599716466D;
+            numericBoxDrawNum.RoundErrorAccuracy = -1;
+            numericBoxDrawNum.ShowUpDown = true;
+            numericBoxDrawNum.SmartIncrement = true;
+            numericBoxDrawNum.Value = 500D;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(graphControl1);
+            resources.ApplyResources(groupBox1, "groupBox1");
+            groupBox1.Name = "groupBox1";
+            groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(graphControl2);
+            resources.ApplyResources(groupBox2, "groupBox2");
+            groupBox2.Name = "groupBox2";
+            groupBox2.TabStop = false;
+            // 
+            // graphControl2
+            // 
+            graphControl2.AllowMouseOperation = true;
+            graphControl2.AxisLineColor = System.Drawing.Color.Gray;
+            graphControl2.AxisTextColor = System.Drawing.Color.Black;
+            graphControl2.AxisTextFont = new System.Drawing.Font("Segoe UI", 9F);
+            graphControl2.AxisXTextVisible = true;
+            graphControl2.AxisYTextVisible = true;
+            graphControl2.BackgroundColor = System.Drawing.Color.White;
+            graphControl2.BottomMargin = 0D;
+            graphControl2.DivisionLineColor = System.Drawing.Color.LightGray;
+            graphControl2.DivisionLineXVisible = true;
+            graphControl2.DivisionLineYVisible = true;
+            resources.ApplyResources(graphControl2, "graphControl2");
+            graphControl2.DrawingRange = (RectangleD)resources.GetObject("graphControl2.DrawingRange");
+            graphControl2.FixRangeHorizontal = false;
+            graphControl2.FixRangeVertical = false;
+            graphControl2.GraphTitle = "";
+            graphControl2.Interpolation = false;
+            graphControl2.IsIntegerX = false;
+            graphControl2.IsIntegerY = false;
+            graphControl2.LabelX = "X:";
+            graphControl2.LabelY = "Y:";
+            graphControl2.LeftMargin = 0F;
+            graphControl2.LineWidth = 1F;
+            graphControl2.LowerX = 0D;
+            graphControl2.LowerY = 0D;
+            graphControl2.MaximalX = 1D;
+            graphControl2.MaximalY = 1D;
+            graphControl2.MinimalX = 0D;
+            graphControl2.MinimalY = 0D;
+            graphControl2.Mode = GraphControl.DrawingMode.Histogram;
+            graphControl2.MousePositionVisible = true;
+            graphControl2.MousePositionXDigit = -1;
+            graphControl2.MousePositionYDigit = -1;
+            graphControl2.Name = "graphControl2";
+            graphControl2.OriginPosition = new System.Drawing.Point(40, 20);
+            graphControl2.Profile = null;
+            graphControl2.Smoothing = false;
+            graphControl2.UnitX = "";
+            graphControl2.UnitY = "";
+            graphControl2.UpperPanelFont = new System.Drawing.Font("Segoe UI Symbol", 9F);
+            graphControl2.UpperPanelVisible = true;
+            graphControl2.UpperX = 1D;
+            graphControl2.UpperY = 1D;
+            graphControl2.UseLineWidth = true;
+            graphControl2.VerticalLineColor = System.Drawing.Color.Red;
+            graphControl2.XLog = false;
+            graphControl2.YLog = false;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripStatusLabel1 });
+            resources.ApplyResources(statusStrip1, "statusStrip1");
+            statusStrip1.Name = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            resources.ApplyResources(toolStripStatusLabel1, "toolStripStatusLabel1");
+            // 
             // FormEBSD
             // 
             resources.ApplyResources(this, "$this");
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(statusStrip1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(numericBoxDrawNum);
+            Controls.Add(numericBoxCalcNum);
             Controls.Add(buttonViewIsometric);
             Controls.Add(buttonViewAlongBeam);
             Controls.Add(numericBoxSampleTilt);
@@ -109,6 +278,10 @@
             Name = "FormEBSD";
             FormClosing += FormEBSD_FormClosing;
             Load += FormEBSD_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -122,5 +295,13 @@
         private System.Windows.Forms.Button buttonViewIsometric;
         private System.Windows.Forms.Button buttonViewAlongBeam;
         private System.Windows.Forms.Panel paneltTrajectory;
+        private GraphControl graphControl1;
+        private NumericBox numericBoxCalcNum;
+        private NumericBox numericBoxDrawNum;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private GraphControl graphControl2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
--- ReciPro/FormEBSD.cs
+++ ReciPro/FormEBSD.cs
@@ -1,9 +1,16 @@
 ﻿using Crystallography.OpenGL;
 using ImagingSolution.Control;
 using IronPython.Runtime;
+using Microsoft.Scripting.Utils;
 using OpenTK;
 using OpenTK.Graphics;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ReciPro;
@@ -14,6 +21,11 @@ public partial class FormEBSD : Form
 
     private GLControlAlpha glControlGeometry;
     private GLControlAlpha glControlTrajectory;
+
+    int seed = 0;
+    object lockObj=new object();
+
+    Stopwatch sw = new();
 
     #region コンストラクタ、Load, Closing
     public FormEBSD()
@@ -36,7 +48,7 @@ public partial class FormEBSD : Form
             LightPosition = new V3(100, 100, 100),
             BorderStyle = BorderStyle.Fixed3D,
 
-            WorldMatrix = Matrix4d.CreateRotationZ(-Math.PI / 8) * Matrix4d.CreateRotationX(-0.4 * Math.PI)
+            WorldMatrix =/* Matrix4d.CreateRotationZ(-Math.PI / 8) * */Matrix4d.CreateRotationX(-0.5 * Math.PI)
         };
         panelGeometry.Controls.Add(glControlGeometry);
 
@@ -47,13 +59,15 @@ public partial class FormEBSD : Form
             AllowMouseTranslating = false,
             Name = "glControlAxes",
             ProjectionMode = GLControlAlpha.ProjectionModes.Orhographic,
-            ProjWidth = 8.0,
+            ProjWidth = 4.0,
             RotationMode = GLControlAlpha.RotationModes.Object,
             Dock = DockStyle.Fill,
             LightPosition = new V3(100, 100, 100),
             BorderStyle = BorderStyle.Fixed3D,
+            DepthCueing = ( false,0,0),
 
-            WorldMatrix = Matrix4d.CreateRotationX(0.5 * Math.PI)
+            WorldMatrix = Matrix4d.CreateRotationX(-0.5 * Math.PI)
+            
         };
         paneltTrajectory.Controls.Add(glControlTrajectory);
 
@@ -132,17 +146,66 @@ public partial class FormEBSD : Form
 
     private void DrawTrajectory()
     {
-        MonteCarlo();
-
-        List<GLObject> glObjects =
-        [
-            new Quads(new V3(5, 5, 0), new V3(-5, 5, 0), new V3(-5, -5, 0), new V3(5, -5, 0), new Material(Color4.AliceBlue, 0.7), DrawingMode.Surfaces)
+        var tilt = numericBoxSampleTilt.RadianValue;
+        List<GLObject> glObjects = [
+            new Disk(new V3(0,0,0),new V3(Math.Sin(tilt),0,Math.Cos(tilt)), 1, new Material(Color4.AliceBlue, 0.7), DrawingMode.Surfaces)
         ];
 
+        var tan = Math.Tan(-numericBoxSampleTilt.RadianValue);
+        var list = new List<(V3 p, double e)[]>();
+        sw.Restart();
+        Parallel.For(0, numericBoxCalcNum.ValueInteger, i =>//for (int i = 0; i < 10000; i++)
+        {
+            var trajectry = MonteCarlo();
+            if (trajectry[^1].p.X * tan < trajectry[^1].p.Z)
+                lock (lockObj)
+                    list.Add(trajectry);
+        });
+
+        toolStripStatusLabel1.Text = $"{sw.ElapsedMilliseconds} ms ellapsed for {numericBoxCalcNum.ValueInteger} trajectories.";
+      
+        int colorDiv = 16;//16段階で色を変化させていく
+        var initialEnergy = waveLengthControl1.Energy;
+        for (int i = 0; i < list.Count && i < numericBoxDrawNum.ValueInteger; i++)
+        {
+            var trajectry = list[i];
+            int start = 0, end=0;
+            for (int j = colorDiv; j >= 1 && end< trajectry.Length; j--)
+            {
+                var mat = new Material(new Color4(255, (byte)(255 * (colorDiv - j) / colorDiv), (byte)(255 * (colorDiv - j) / colorDiv), (byte)(255 * j / colorDiv)));
+
+                end = trajectry.FindIndex(t => t.e < (double)(j - 1) / colorDiv * initialEnergy);
+                if (end == -1)
+                    end = trajectry.Length;
+                glObjects.Add(new Lines(trajectry[start..end].Select(e => e.p).ToArray(), 1f, mat));
+             
+                start = end - 1;
+            }
+
+            var r = trajectry[^2].e / waveLengthControl1.Energy;
+            var v = (trajectry[^1].p - trajectry[^2].p).Normalized() * r;
+            var matBackScattered = new Material(new Color4((byte)(255 * (1 - r)), (byte)(255 * (1 - r)), 255, (byte)(255 * r)));
+
+            glObjects.Add(new Lines([trajectry[^2].p, trajectry[^2].p + v], 1f, matBackScattered));
+        }
 
         glControlTrajectory.DeleteAllObjects();
         glControlTrajectory.AddObjects(glObjects);
         glControlTrajectory.Refresh();
+
+        //エネルギー分布を描画 ここから
+        double step = 0.2;//kev単位
+        double lower = 2, upper = initialEnergy;
+        var histogram = new MathNet.Numerics.Statistics.Histogram(list.Select(e => e[^1].e), (int)((initialEnergy - lower) / step), 2, initialEnergy);
+        var pts = new List<PointD>();
+        for (int i = 0; i < histogram.BucketCount; i++)
+            pts.Add(new PointD((histogram[i].UpperBound + histogram[i].LowerBound) / 2, (double)histogram[i].Count / list.Count));
+        pts.Add(new PointD(initialEnergy + step / 2, 0));
+        graphControl1.ClearProfile();
+        graphControl1.Profile = new Profile(pts);
+        graphControl1.MaximalX = initialEnergy * 1.05;
+        graphControl1.UpperX = graphControl1.MaximalX;
+        //エネルギー分布を描画 ここまで
     }
 
 
@@ -152,49 +215,68 @@ public partial class FormEBSD : Form
         //Eqs (2.38), (2.41), (2.42) などを参考
 
         //ここから、シミュレーション中に変化しない変数を定義
-        //試料の平均原子番号
-        //各元素の重量比で加重平均
-        var Z = 29;
+        //試料の平均原子番号. 各元素の重量比で加重平均
+        var Z = 13;// 79 29 13;
         //試料の平均原子量 (g/mol)
-        var A = 63.546;
+        var A = 26.98; //196.96 63.55 26.98;
         //試料の密度 (g/cm^3)
-        var ρ = 8.94;// FormMain.Crystal.Density;
-        var ρ_gram_per_nm3 = 8.94E-21;
+        var ρ = 2.70; // 19.32 8.96 2.70 //FormMain.Crystal.Density;
 
         //トータル散乱断面積の計算中に出てくる定数
         var coeff1 = Z * UniversalConstants.e0 * UniversalConstants.e0 / (8.0 * Math.PI * UniversalConstants.ε0);
         //平均自由行程のところに出てくる定数
-        var coeff2 = A / UniversalConstants.A / ρ_gram_per_nm3 / Math.PI;
+        var coeff2 = A / UniversalConstants.A / ρ * 1E21;// / Math.PI;
         //阻止能の計算中に出てくる定数
-        var coeff3 = -Z * UniversalConstants.A * ρ * 1E3 / (A * 1E-3) * Math.Pow(UniversalConstants.e0, 4) 
-            / 4 / Math.PI / UniversalConstants.ε0 / UniversalConstants.ε0 / UniversalConstants.eV_joule * 1E-9;
+        var coeff3 = -Z * UniversalConstants.A * ρ * 1E3 / (A * 1E-3) * Math.Pow(UniversalConstants.e0, 4)
+            / 4 / Math.PI / UniversalConstants.ε0 / UniversalConstants.ε0 / UniversalConstants.eV_joule * 1E-9 * 1E-3;
         //阻止能の計算中に出てくる物質依存の定数 k    Joy and Luo (1989)によれば 6C:0.77, 13Al: 0.815, 14Si: 0.822, 28Ni: 0.83, 29Cu: 0.83,  47Ag:0.852, 79Au: 0.851
         //取りあえず対数近似した値を使う
         var k = 0.0299 * Math.Log(Z) + 0.7307;
         //阻止能の計算中に出てくる物質依存の定数 J (eV) Z<=12の時は J=11.5evにするらしい (Joy&Luo 1989)
-        var J = Z<= 12 ? 11.5 * Z : (9.76 * Z + 58.5 / Math.Pow(Z, 0.19));
+        var J = Z <= 12 ? 11.5 * Z : (9.76 * Z + 58.5 / Math.Pow(Z, 0.19));
         //ここまで、シミュレーション中に変化しない変数を定義
 
-        //電子エネルギー (kev) 初期値はwaveLengthControlから取得
-        var kev = waveLengthControl1.Energy;
+        var r = new Random(Interlocked.Increment(ref seed));
+
+        var trajectory = new List<(V3 p, double e)>() { (new V3(0, 0, 0), waveLengthControl1.Energy) };
+        var tan = Math.Tan(-numericBoxSampleTilt.RadianValue);
+        var vec = new V3(0, 0, -1);
 
         //電子エネルギーが2kev以下になるまでループ
-        while (kev > 2)
+        while (trajectory[^1].e > 2 && trajectory[^1].p.X * tan >= trajectory[^1].p.Z)
         {
             //電子の質量 (kg) × 電子の速度の2乗 (m^2/s^2)
-            var mv2 = UniversalConstants.Convert.EnergyToElectronMass(kev) * UniversalConstants.Convert.EnergyToElectronVelositySquared(kev);
+            var mv2 = UniversalConstants.Convert.EnergyToElectronMass(trajectory[^1].e) * UniversalConstants.Convert.EnergyToElectronVelositySquared(trajectory[^1].e);
             //散乱係数
-            var α = 0.0034 * Math.Pow(Z, 2.0 / 3.0) / kev;
+            var α = 0.0034 * Math.Pow(Z, 2.0 / 3.0) / trajectory[^1].e;
             //トータル散乱断面積 (nm^2)
             var t1 = coeff1 / mv2;
             var σ_E = t1 * t1 * 4 * Math.PI / α / (α + 1) * 1E18;
-            //弾性散乱平均自由行程 (nm)  注(2.33)の式は分母のpiが抜けているっぽい。。たぶん。。
+            //弾性散乱平均自由行程 (nm) 
             var λ_el = coeff2 / σ_E;
-            //阻止能 (Joy and Luo 1989) (ev/nm単位)
+            //阻止能 (Joy and Luo 1989) (kev/nm単位)
             var sp = coeff3 / mv2 * Math.Log(1.166 * k + 0.583 * mv2 / UniversalConstants.eV_joule / J);
             //var sp2 = -7.85 * ρ  * Z / A / kev  * Math.Log(1.166 * kev / J);
+
+            //乱数発生
+            double rnd1 = r.NextDouble();
+
+            //飛行距離 s
+            var s = -λ_el * Math.Log(rnd1);
+
+            if (trajectory.Count == 1)
+                trajectory.Add((s * vec, trajectory[^1].e + s * sp));
+            else
+            {
+                double rnd2 = r.NextDouble(), rnd3 = r.NextDouble();
+                double cosθ = 1 - 2 * α * rnd2 / (1 + α - rnd2), sinθ = Math.Sqrt(1 - cosθ * cosθ);
+                var φ = 2 * Math.PI * rnd3;
+                var rot = GLGeometry.CreateRotationFromZ(vec);
+                vec = rot.Mult(new V3(sinθ * Math.Cos(φ), sinθ * Math.Sin(φ), cosθ));
+                trajectory.Add((trajectory[^1].p + s * vec, trajectory[^1].e + s * sp));
+            }
         }
-        return null;
+        return trajectory.Select(e => (e.p / 1000, e.e)).ToArray();
     }
 
     #endregion
