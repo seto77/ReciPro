0f7739459ef2dd8937d16c287396f7da2c09b760
seto <seto77@live.jp>
2024-11-14 08:58:51 +0900

    update

--- ReciPro/Version.cs
+++ ReciPro/Version.cs
@@ -7,6 +7,7 @@ internal static class Version
 
     public const string History =
         "History" +
+        "\r\n ver4.895(2024/11/14)  Updated: .Net Desktop Runtime 8.0 to 9.0. Updated the bundled crystal database." +
         "\r\n ver4.894(2024/11/01)  Fixed bugs on the 'Diffraction Simulator' and 'HRTEM/STEM simulator' (thanks to lukmuk-san and Nakamura-san!)." +
         "\r\n ver4.892(2024/10/04)  Added several 'Macro' functions." +
         "\r\n ver4.891(2024/09/06)  Added the function to simulate electron trajectories based on the Monte Carlo method." +
